dbf57fd3d12c25f6ce3d339c65f14d29250ef4e0
Your Name <you@example.com>
2025-12-01 12:29:27 +0800

    feat: Enable Swagger and SwaggerUI globally.

--- Program.cs
+++ Program.cs
@@ -98,10 +98,12 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
 
 // app.UseHttpsRedirection();  //disabled for docker
